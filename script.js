1962b22638b2c8f24b7f17664f677ab00ec870fe
Bojan-dev <bojanvreco069@gmail.com>
2020-12-06 00:52:08 +0100

    New changes

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const heroSocialNetworks = document.querySelector('.hero-social-networks');
 
 heroSocialNetworks.addEventListener('mouseover', () => {
   heroSocialNetworks.style.right = '0';
+  heroSocialNetworks.style.transition = '0.75s ease-in-out';
 });
 
 heroSocialNetworks.addEventListener('mouseout', () => {
   heroSocialNetworks.style.right = '-205px';
 });
+
+//Each wrapper
+const curoselDivs = document.querySelectorAll('.benefit-wrapper');
+
+//Buttons
+const prevBtn = document.querySelector('.benefits-slider-icon-left');
+const nextBtn = document.querySelector('benefits-slider-icon-right');
